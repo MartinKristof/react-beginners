88248b9f0967a77a459e5af056dab830239d6c3c
Martin Kristof <martin.kristof@heureka.cz>
2024-01-29 20:07:56 +0100

    chore: prepare dir structure

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export const App = () => (
+  <>
+    <h1 className="text-3xl font-bold underline">Wall</h1>
+    <div className="my-2">
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  </>
+);
